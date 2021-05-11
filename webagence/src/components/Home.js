725176d46f8563ded9c2c8a9247c12fbd693e9ee
Janainaf <figueira.janaina@gmail.com>
2021-05-11 21:27:31 +0200

    adding routes

---
+++ webagence/src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="main-content home">
+        <h2>Web Agence </h2>
+        <p>
+          This fun agence website is a project for the{" "}
+          <em> Advanced React (Context and Router)</em> course on The Hacking
+          Project{" "}
+        </p>
+        <p>
+          This agence is especialised on front end web development and much
+          more!
+        </p>
+        <p>
+          If you want to know ,more about the service we provide, navigate
+          through the page. And hava fun :)
+        </p>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Home;
