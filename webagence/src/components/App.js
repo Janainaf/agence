7a905bbac075b9a7e0f888d900de3652c49cec99
Janainaf <figueira.janaina@gmail.com>
2021-05-11 21:36:03 +0200

    routing home

--- webagence/src/components/App.js
+++ webagence/src/components/App.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./Home";
 
-const App = () => <div className="container"></div>;
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Route path="/" component={Home} />
+    </div>
+    ;
+  </BrowserRouter>
+);
 
 export default App;
