da7081859dae71d6a47c2bab3c1d296ef939d36c
Janainaf <figueira.janaina@gmail.com>
2021-05-11 21:40:47 +0200

    routing about section

---
+++ webagence/src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => (
+  <div className="main-content">
+    <h2>About</h2>
+    <p>
+      This agence is more than an agence! Be sure to visit ur WORKS section to
+      view a list of our best projects.
+    </p>
+  </div>
+);
+
+export default About;
--- webagence/src/components/App.js
+++ webagence/src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./Home";
+import About from "./About";
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
     </div>
     ;
   </BrowserRouter>
